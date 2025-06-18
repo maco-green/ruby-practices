af5c69f887befbdcd2abb9bd1eefbf8efd9d445e
maco_green <version.k0824@gmail.com>
2025-06-18 11:50:11 +0900

    FizzBuzz解答

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,13 @@
+count = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+  count.each do |x|
+    if x % 3 == 0 && x % 5 == 0
+      puts "FizzBuzz"
+    elsif x % 3 == 0
+      puts "Fizz"
+    elsif x % 5 == 0
+      puts "Buzz"
+    else
+      puts x
+    end
+end
