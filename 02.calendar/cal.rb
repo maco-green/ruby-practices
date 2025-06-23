b16683985a389556998e8e4130c96706524bbe02
maco_green <version.k0824@gmail.com>
2025-06-23 13:51:56 +0900

    カレンダープログラム(Ruby)の提出

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require "date"
+require 'optparse'
+
+year = Date.today.year
+month = Date.today.month
+
+OptionParser.new do |option|
+    option.on("-y", "--year YEAR", Integer, "Year") { |v| year = v }
+    option.on("-m", "--month MONTH", Integer, "Month") { |v| month = v }
+end.parse!
+
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
+
+puts "#{month}月 #{year}".center(18)
+puts "日 月 火 水 木 金 土"
+print "   " * first_day.wday
+
+(1..last_day.day).each do |day|
+    print "%2d " % day
+    if (first_day.wday + day) % 7 == 0
+        puts
+    end
+end
+
+
+puts
