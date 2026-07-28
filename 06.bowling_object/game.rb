c36daa99c66209fa31fa945d203e96a885ce527c
maco_green <version.k0824@gmail.com>
2026-07-28 17:54:33 +0900

    GameがFrameの配列を持つように変更

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -4,40 +4,37 @@ require_relative 'frame'
 
 class Game
   def initialize(marks)
-    @marks = marks.split(',')
-  end
-
-  def frames
-    @frames ||= begin
+    marks = marks.split(',')
+    @frames = begin
       result = []
       until result.size == 9
-        result << if @marks[0] == 'X'
-                    @marks.shift(1)
+        result << if marks[0] == 'X'
+                    marks.shift(1)
                   else
-                    @marks.shift(2)
+                    marks.shift(2)
                   end
       end
-      result << @marks
+      result << marks
       result.map { |marks| Frame.new(marks) }
     end
   end
 
   def total_score
     total = 0
-    frames.first(9).each_with_index do |frame, index|
+    @frames.first(9).each_with_index do |frame, index|
       total += if frame.strike?
-                 if frames[index + 1].shot_size == 1
-                   frame.score + frames[index + 1].score + frames[index + 2].first_pins
+                 if @frames[index + 1].shot_size == 1
+                   frame.score + @frames[index + 1].score + @frames[index + 2].first_pins
                  else
-                   frame.score + frames[index + 1].first_pins + frames[index + 1].second_pins
+                   frame.score + @frames[index + 1].first_pins + @frames[index + 1].second_pins
                  end
                elsif frame.spare?
-                 frame.score + frames[index + 1].first_pins
+                 frame.score + @frames[index + 1].first_pins
                else
                  frame.score
                end
     end
-    total += frames[9].score
+    total += @frames[9].score
     total
   end
 end
